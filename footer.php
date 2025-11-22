46aa2a062bc06addd08fa270901e82d604595b62
Roozbeh Yeganeh <roozbeh1985@gmail.com>
2025-11-22 13:28:04 +0330

    ok

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <script src="<?php bloginfo('template_url'); ?>/js/sweetalert2.js"></script>
 <?php wp_footer(); ?>
-<!-- <script src="<?php bloginfo('template_url'); ?>/swiper/js/swiper.min1.js"></script>
-<script src="<?php bloginfo('template_url'); ?>/js/jquery.min.js"></script> -->
+<script src="<?php bloginfo('template_url'); ?>/swiper/js/swiper.min1.js"></script>
+<!-- <script src="<?php bloginfo('template_url'); ?>/js/jquery.min.js"></script> -->
 </body>
 </html>

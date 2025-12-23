0c7c22d896472fece9c0e8548da836189ad7b0a8
Roozbeh Yeganeh <roozbeh1985@gmail.com>
2025-12-23 09:43:08 +0330

    خ

--- header.php
+++ header.php
@@ -28,16 +28,7 @@
     <script src="<?php bloginfo('template_url'); ?>/js/jquery.lazy.min.js"></script>
     <link rel="icon" type="image/x-icon" href="https://4khooneh.org/favicon.ico">
 
-    <!-- Google Tag Manager -->
-    <script>(function (w, d, s, l, i) {
-            w[l] = w[l] || []; w[l].push({
-                'gtm.start':
-                    new Date().getTime(), event: 'gtm.js'
-            }); var f = d.getElementsByTagName(s)[0],
-                j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
-                    'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
-        })(window, document, 'script', 'dataLayer', 'GTM-TW9C7H4M');</script>
-    <!-- End Google Tag Manager -->
+   
     <script>
         !function (t, e, n) {
             t.yektanetAnalyticsObject = n, t[n] = t[n] || function () {
--- test-php.php
+++ test-php.php
@@ -1,4 +1,100 @@
 <?php
-
-?>
-s:1445:"a:6:{i:0;a:5:{s:4:"link";s:46:"https://4khooneh.org/آموزش-و-پرورش/";s:3:"img";s:56:"https://4khooneh.org/wp-content/uploads/2025/12/hp53.png";s:3:"txt";s:24:"آموزش و پرورش";s:4:"main";s:2:"ok";s:5:"color";s:7:"#c1ccd2";}i:1;a:5:{s:4:"link";s:45:"https://4khooneh.org/استخدام-بانک/";s:3:"img";s:56:"https://4khooneh.org/wp-content/uploads/2025/12/hp54.png";s:3:"txt";s:8:"بانک";s:4:"main";s:2:"ok";s:5:"color";s:7:"#c1ccd2";}i:2;a:5:{s:4:"link";s:62:"https://4khooneh.org/استخدام-دستگاه-اجرایی/";s:3:"img";s:56:"https://4khooneh.org/wp-content/uploads/2025/12/hp52.png";s:3:"txt";s:25:"دستگاه اجرایی";s:4:"main";s:2:"ok";s:5:"color";s:7:"#c1ccd2";}i:3;a:5:{s:4:"link";s:53:"https://4khooneh.org/استخدام-پتروشیمی/";s:3:"img";s:56:"https://4khooneh.org/wp-content/uploads/2025/12/hp50.png";s:3:"txt";s:16:"پتروشیمی";s:4:"main";s:2:"ok";s:5:"color";s:7:"#c1ccd2";}i:4;a:5:{s:4:"link";s:60:"https://4khooneh.org/استخدام-وزارت-بهداشت/";s:3:"img";s:56:"https://4khooneh.org/wp-content/uploads/2025/12/hp51.png";s:3:"txt";s:23:"وزارت بهداشت";s:4:"main";s:2:"ok";s:5:"color";s:7:"#c1ccd2";}i:5;a:5:{s:4:"link";s:52:"https://4khooneh.org/سایر-استخدامی-ها/";s:3:"img";s:56:"https://4khooneh.org/wp-content/uploads/2022/12/hp42.png";s:3:"txt";s:24:"سایر ارگان ها";s:4:"main";s:2:"ok";s:5:"color";s:7:"#c1ccd2";}}";
+array (
+  0 => 
+  array (
+    'link' => 'https://4khooneh.org/%d8%a2%d9%85%d9%88%d8%b2%d8%b4-%d9%88-%d9%be%d8%b1%d9%88%d8%b1%d8%b4/',
+    'img' => 'https://4khooneh.org/wp-content/uploads/2025/12/hp53.png',
+    'txt' => ' آموزش و پرورش',
+    'main' => 'ok',
+    'color' => '#c1ccd2',
+  ),
+  1 => 
+  array (
+    'link' => 'https://4khooneh.org/%d8%a7%d8%b3%d8%aa%d8%ae%d8%af%d8%a7%d9%85-%d8%a8%d8%a7%d9%86%da%a9/',
+    'img' => 'https://4khooneh.org/wp-content/uploads/2025/12/hp54.png',
+    'txt' => 'بانک',
+    'main' => 'ok',
+    'color' => '#c1ccd2',
+  ),
+  2 => 
+  array (
+    'link' => 'https://4khooneh.org/%d8%a7%d8%b3%d8%aa%d8%ae%d8%af%d8%a7%d9%85-%d8%af%d8%b3%d8%aa%da%af%d8%a7%d9%87-%d8%a7%d8%ac%d8%b1%d8%a7%db%8c%db%8c/',
+    'img' => 'https://4khooneh.org/wp-content/uploads/2025/12/hp52.png',
+    'txt' => 'دستگاه اجرایی',
+    'main' => 'ok',
+    'color' => '#c1ccd2',
+  ),
+  3 => 
+  array (
+    'link' => 'https://4khooneh.org/%d8%a7%d8%b3%d8%aa%d8%ae%d8%af%d8%a7%d9%85-%d9%be%d8%aa%d8%b1%d9%88%d8%b4%db%8c%d9%85%db%8c/',
+    'img' => 'https://4khooneh.org/wp-content/uploads/2025/12/hp50.png',
+    'txt' => 'پتروشیمی',
+    'main' => 'ok',
+    'color' => '#c1ccd2',
+  ),
+  4 => 
+  array (
+    'link' => 'https://4khooneh.org/%d8%a7%d8%b3%d8%aa%d8%ae%d8%af%d8%a7%d9%85-%d9%88%d8%b2%d8%a7%d8%b1%d8%aa-%d8%a8%d9%87%d8%af%d8%a7%d8%b4%d8%aa/',
+    'img' => 'https://4khooneh.org/wp-content/uploads/2025/12/hp51.png',
+    'txt' => ' وزارت بهداشت ',
+    'main' => 'ok',
+    'color' => '#c1ccd2',
+  
+  ),
+  5 => 
+  array (
+    'link' => 'https://4khooneh.org/%d8%b3%d8%a7%db%8c%d8%b1-%d8%a7%d8%b3%d8%aa%d8%ae%d8%af%d8%a7%d9%85%db%8c-%d9%87%d8%a7/',
+    'img' => 'https://4khooneh.org/wp-content/uploads/2022/12/hp42.png',
+    'txt' => ' سایر ارگان ها ',
+    'main' => 'ok',
+    'color' => '#c1ccd2',
+  ),
+  6 => 
+  array (
+    'link' => 'https://4khooneh.org/%d8%b3%d8%a7%db%8c%d8%b1-%d8%a7%d8%b3%d8%aa%d8%ae%d8%af%d8%a7%d9%85%db%8c-%d9%87%d8%a7/',
+    'img' => 'https://4khooneh.org/wp-content/uploads/2022/12/hp42.png',
+    'txt' => ' سایر ارگان ها ',
+    'main' => 'ok',
+    'color' => '#c1ccd2',
+  ),
+  7 => 
+  array (
+    'link' => 'https://4khooneh.org/%d8%b3%d8%a7%db%8c%d8%b1-%d8%a7%d8%b3%d8%aa%d8%ae%d8%af%d8%a7%d9%85%db%8c-%d9%87%d8%a7/',
+    'img' => 'https://4khooneh.org/wp-content/uploads/2022/12/hp42.png',
+    'txt' => ' سایر ارگان ها ',
+    'main' => 'ok',
+    'color' => '#c1ccd2',
+  ),
+  8 => 
+  array (
+    'link' => 'https://4khooneh.org/%d8%b3%d8%a7%db%8c%d8%b1-%d8%a7%d8%b3%d8%aa%d8%ae%d8%af%d8%a7%d9%85%db%8c-%d9%87%d8%a7/',
+    'img' => 'https://4khooneh.org/wp-content/uploads/2022/12/hp42.png',
+    'txt' => ' سایر ارگان ها ',
+    'main' => 'ok',
+    'color' => '#c1ccd2',
+  ),
+  9 => 
+  array (
+    'link' => 'https://4khooneh.org/%d8%b3%d8%a7%db%8c%d8%b1-%d8%a7%d8%b3%d8%aa%d8%ae%d8%af%d8%a7%d9%85%db%8c-%d9%87%d8%a7/',
+    'img' => 'https://4khooneh.org/wp-content/uploads/2022/12/hp42.png',
+    'txt' => ' سایر ارگان ها ',
+    'main' => 'ok',
+    'color' => '#c1ccd2',
+  ),
+  10 => 
+  array (
+    'link' => 'https://4khooneh.org/%d8%b3%d8%a7%db%8c%d8%b1-%d8%a7%d8%b3%d8%aa%d8%ae%d8%af%d8%a7%d9%85%db%8c-%d9%87%d8%a7/',
+    'img' => 'https://4khooneh.org/wp-content/uploads/2022/12/hp42.png',
+    'txt' => ' سایر ارگان ها ',
+    'main' => 'ok',
+    'color' => '#c1ccd2',
+  ),
+  11 => 
+  array (
+    'link' => 'https://4khooneh.org/%d8%b3%d8%a7%db%8c%d8%b1-%d8%a7%d8%b3%d8%aa%d8%ae%d8%af%d8%a7%d9%85%db%8c-%d9%87%d8%a7/',
+    'img' => 'https://4khooneh.org/wp-content/uploads/2022/12/hp42.png',
+    'txt' => ' سایر ارگان ها ',
+    'main' => 'ok',
+    'color' => '#c1ccd2',
+  ),
+);

fe57e22eb01a693bb763eee6f879af1a83c3760f
Roozbeh Yeganeh <roozbeh1985@gmail.com>
2025-12-28 10:20:47 +0330

    ok

--- header.php
+++ header.php
@@ -21,7 +21,7 @@
     <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/all.min.css">
     <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/css/ck-menustyle.css?v=1.0.0.5">
     <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/css/examples.css?v=1.0.0.2" />
-    <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/style.css?v=1.9.4.088">
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/style.css?v=1.9.4.089">
     <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/swiper/css/swiper.min.css">
 
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
